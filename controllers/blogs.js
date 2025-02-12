03a84e3be8627f475da818caf74fbba046e5a875
JuampiConejera <juampiconejera@gmail.com>
2025-02-12 18:45:27 -0300

    commit exercise 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const { request } = require('express')
 
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
@@ -68,6 +69,27 @@ blogsRouter.put('/:id', async (request, response) => {
   
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params
+  const decodedToken = jwt.verify(request.token, proccess.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid '})
+  }
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(id).populate('user', { id: 1 })
+  
+  if (!user) {
+    response.status(400).json({ error: 'User does not exist' })
+  }
+
+  if (user._id.toString() !== blog.user._id.toString()) {
+    return response.status(401).json({ error: 'Not authorized to do that', user: user._id, blogUser: blog.user.id})
+  }
+
+  await Blog.findByIdAndRemove(id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
